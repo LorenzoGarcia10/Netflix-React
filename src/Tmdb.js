3976233d7b13920d0b885101b494e6b93c8f9bd3
unknown <Loren@LorenzoGarcia.BLUETI.LOCAL>
2024-05-20 16:02:35 -0400

    first commit

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -10,14 +10,15 @@ const API_BASE = 'https://api.themoviedb.org/3';
 - terror
 - romance
 - documentarios
-*/ 
-const basicFetch = async (endpoint) =>{
-    const req = await fetch(`${API_BASE}${endpoint}`)
-    const json = await req.json()
-    return json
-}
-export default { 
+*/
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+};
 
+// Correção na linha 19: atribuir o objeto a uma variável antes de exportá-lo
+const tmdbAPI = {
     getHomeList: async () => {
         return [
             {
@@ -37,7 +38,7 @@ export default {
             },
             {
                 slug: 'action',
-                title: 'Ação', 
+                title: 'Ação',
                 items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
@@ -59,30 +60,30 @@ export default {
                 slug: 'documentary',
                 title: 'Documentários',
                 items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
-            },
-
-
+            }
         ];
     },
 
-        getMovieInfo: async (movieId, type)=>{
-            let info = {}
-        
-            if(movieId){
-                switch(type){
-                    case 'movie':
-                        info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`)
-                    break
-                    case 'tv': 
-                    info = await basicFetch(`/tv/${movieId}?
-                    language=pt-BR&api_key=${API_KEY}`)
-                    break
-                }
-            }
-            
-            return info
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
 
+        if (movieId) {
+            switch (type) {
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                default:
+                    // Adiciona um caso default para lidar com tipos inesperados
+                    info = null;
+                    break;
+            }
         }
 
- 
-}
+        return info;
+    }
+};
+
+export default tmdbAPI;
